b768d74575fab29dde551a6194f55b5824e71f37
afujinawa1 <afujinawa1>
2023-06-23 18:23:50 +0100

    adj

--- particleNS/Source/particleFunc.cpp
+++ particleNS/Source/particleFunc.cpp
@@ -323,20 +323,20 @@ AmrLevelAdv::updateParticleInfo(MultiFab& Sborder, const double& dt, const doubl
         if (particle == 2){ // if we enable two-way coupling
             vCell = dx*interDist*interDist;
             for (int h = 0; h < NUM_STATE; h++){
-                std::cout << "Gas: rho rhou rhov e o2 n2" << arr(i,j,k,0) << " " << arr(i,j,k,1) << \
-                " " << arr(i,j,k,2) << " " << arr(i,j,k,3) << " " << arr(i,j,k,4) << " " << arr(i,j,k,5) << std::endl;
-                std::cout << "Source: rho rhou rhov e o2 n2" << qSource[0] << " " << qSource[1] << \
-                " " << qSource[2] << " " << qSource[3] << " " << qSource[4] << " " << qSource[5] << std::endl;
+                // std::cout << "Gas: rho rhou rhov e o2 n2" << arr(i,j,k,0) << " " << arr(i,j,k,1) << \
+                // " " << arr(i,j,k,2) << " " << arr(i,j,k,3) << " " << arr(i,j,k,4) << " " << arr(i,j,k,5) << std::endl;
+                // std::cout << "Source: rho rhou rhov e o2 n2" << qSource[0] << " " << qSource[1] << \
+                // " " << qSource[2] << " " << qSource[3] << " " << qSource[4] << " " << qSource[5] << std::endl;
                     
                     
-                if (arr(i,j,k,h) != arr(i,j,k,h)){
-                    std::cout << "Nan BEFORE applying LagSource, \n rho rhou rhov e o2 n2" << arr(i,j,k,0) << " " << arr(i,j,k,1) << \
-                    " " << arr(i,j,k,2) << " " << arr(i,j,k,3) << " " << arr(i,j,k,4) << " " << arr(i,j,k,5) << std::endl;
-                    std::cout << "qSrc: rho rhou rhov e o2 n2" << qSource[0] << " " << qSource[1] << \
-                    " " << qSource[2] << " " << qSource[3] << " " << qSource[4] << " " << qSource[5] << std::endl;
-                    std::cout << "Nan found after getSource, before particle to gas update, variable h: " << h << std::endl;
-                    Abort("nan found");
-                }
+                // if (arr(i,j,k,h) != arr(i,j,k,h)){
+                //     std::cout << "Nan BEFORE applying LagSource, \n rho rhou rhov e o2 n2" << arr(i,j,k,0) << " " << arr(i,j,k,1) << \
+                //     " " << arr(i,j,k,2) << " " << arr(i,j,k,3) << " " << arr(i,j,k,4) << " " << arr(i,j,k,5) << std::endl;
+                //     std::cout << "qSrc: rho rhou rhov e o2 n2" << qSource[0] << " " << qSource[1] << \
+                //     " " << qSource[2] << " " << qSource[3] << " " << qSource[4] << " " << qSource[5] << std::endl;
+                //     std::cout << "Nan found after getSource, before particle to gas update, variable h: " << h << std::endl;
+                //     Abort("nan found");
+                // }
                 arr(i,j,k,h) = arr(i,j,k,h) + dt*(1.0/vCell)*qSource[h];
                 if (arr(i,j,k,h) != arr(i,j,k,h)){
                     std::cout << "Nan AFTER applying LagSource, \n rho rhou rhov e o2 n2" << arr(i,j,k,0) << " " << arr(i,j,k,1) << \
@@ -624,7 +624,9 @@ void getSource(Vector<double>& qSource, Vector<double>& pSource, const Vector<do
 
     dHgOxidt = (mdotFe/M_Fe)*(0.5*qFe2O3s*M_Fe2O3) + (mdotFeO/M_FeO)*(0.5*qFe2O3s*M_Fe2O3-qFeOg*M_FeO); 
 
-    // std::cout << "dmO2dt: " << dmO2dt << ", dupdt: " << dupdt << ", dHgOxidt: " << dHgOxidt << std::endl;
+    std::cout << "mFe: " << mFe << ", mFeO: " << mFeO << ", mFe3O4: " << mFe3O4 << std::endl;
+    std::cout << "dupdt: " << dupdt << ", dvpdt: " << dvpdt << std::endl;
+    std::cout << "dmO2dt: " << dmO2dt << ", dupdt: " << dupdt << ", dHgOxidt: " << dHgOxidt << std::endl;
 
     qSource[gasVar::rho]    = dmO2dt;
     qSource[gasVar::rhou]   = -(mFe+mFeO+mFe3O4)*dupdt;
