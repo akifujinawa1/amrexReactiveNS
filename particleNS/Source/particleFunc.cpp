147ac41121f51c87ccbac9a9fc2693b4ac103c4a
afujinawa1 <afujinawa1>
2023-06-23 17:47:26 +0100

    adjusted nan checker

--- particleNS/Source/particleFunc.cpp
+++ particleNS/Source/particleFunc.cpp
@@ -323,9 +323,17 @@ AmrLevelAdv::updateParticleInfo(MultiFab& Sborder, const double& dt, const doubl
         if (particle == 2){ // if we enable two-way coupling
             vCell = dx*interDist*interDist;
             for (int h = 0; h < NUM_STATE; h++){
+                if (arr(i,j,k,h) != arr(i,j,k,h)){
+                    std::cout << "Nan BEFORE applying LagSource, \n rho rhou rhov e o2 n2" << arr(i,j,k,0) << " " << arr(i,j,k,1) << \
+                    " " << arr(i,j,k,2) << " " << arr(i,j,k,3) << " " << arr(i,j,k,4) << " " << arr(i,j,k,5) << std::endl;
+                    std::cout << "qSrc: rho rhou rhov e o2 n2" << qSource[0] << " " << qSource[1] << \
+                    " " << qSource[2] << " " << qSource[3] << " " << qSource[4] << " " << qSource[5] << std::endl;
+                    std::cout << "Nan found after getSource, before particle to gas update, variable h: " << h << std::endl;
+                    Abort("nan found");
+                }
                 arr(i,j,k,h) = arr(i,j,k,h) + dt*(1.0/vCell)*qSource[h];
                 if (arr(i,j,k,h) != arr(i,j,k,h)){
-                    std::cout << "rho rhou rhov e o2 n2" << arr(i,j,k,0) << " " << arr(i,j,k,1) << \
+                    std::cout << "Nan AFTER applying LagSource, \n rho rhou rhov e o2 n2" << arr(i,j,k,0) << " " << arr(i,j,k,1) << \
                     " " << arr(i,j,k,2) << " " << arr(i,j,k,3) << " " << arr(i,j,k,4) << " " << arr(i,j,k,5) << std::endl;
                     std::cout << "qSrc: rho rhou rhov e o2 n2" << qSource[0] << " " << qSource[1] << \
                     " " << qSource[2] << " " << qSource[3] << " " << qSource[4] << " " << qSource[5] << std::endl;
