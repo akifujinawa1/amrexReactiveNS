53ff69e7adcd959e690dc4967037d737f1a35a4e
A. Fujinawa <af793@cerberus1.lsc.phy.cam.ac.uk>
2023-06-26 15:34:46 +0100

    new print files

--- particleNS/Exec/UniformVelocity/tmp_build_dir/o/1d.gnu.MPI.EXE/AMReX_BoxDomain.d
+++ particleNS/Exec/UniformVelocity/tmp_build_dir/o/1d.gnu.MPI.EXE/AMReX_BoxDomain.d
@@ -4,6 +4,8 @@ tmp_build_dir/o/1d.gnu.MPI.EXE/AMReX_BoxDomain.o: \
  tmp_build_dir/s/1d.gnu.MPI.EXE/AMReX_Config.H \
  ../../../../amrex/Src/Base/AMReX_IndexType.H \
  ../../../../amrex/Src/Base/AMReX_ccse-mpi.H \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h \
+ /usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h \
  ../../../../amrex/Src/Base/AMReX_IntVect.H \
  ../../../../amrex/Src/Base/AMReX_INT.H \
  ../../../../amrex/Src/Base/AMReX_SPACE.H \
@@ -44,6 +46,10 @@ tmp_build_dir/s/1d.gnu.MPI.EXE/AMReX_Config.H:
 ../../../../amrex/Src/Base/AMReX_IndexType.H:
 
 ../../../../amrex/Src/Base/AMReX_ccse-mpi.H:
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi.h:
+
+/usr/lib/x86_64-linux-gnu/openmpi/include/mpi_portable_platform.h:
 
 ../../../../amrex/Src/Base/AMReX_IntVect.H:
 
