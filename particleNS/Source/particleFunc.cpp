2891fe4b7ed4303ec5ddf4db78912246166f33eb
afujinawa1 <afujinawa1>
2023-07-05 18:44:14 +0100

    changed initial temperature to 1450 K

--- particleNS/Source/particleFunc.cpp
+++ particleNS/Source/particleFunc.cpp
@@ -136,8 +136,8 @@ AmrLevelAdv::initParticles (const MultiFab& S_new, const double& xDisc, const do
             
             if (enIC==14){
                 // if ((p.pos(0)/10e-6) < n_cell*0.1){
-                if (p.pos(0) < 2*xDisc){
-                    energy0 = Hparticle(mFe0,mFeO0,mFe3O40,1270,0,0,0);
+                if (p.pos(0) < xDisc){
+                    energy0 = Hparticle(mFe0,mFeO0,mFe3O40,1450,0,0,0);
                 }
             }    
 
