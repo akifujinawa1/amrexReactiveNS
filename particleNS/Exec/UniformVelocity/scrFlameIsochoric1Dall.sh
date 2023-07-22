d6c24ce6f01c6ef7358f0e4908c21ffd06c91b4d
afujinawa1 <afujinawa1>
2023-07-22 23:08:46 +0100

    go again

--- particleNS/Exec/UniformVelocity/scrFlameIsochoric1Dall.sh
+++ particleNS/Exec/UniformVelocity/scrFlameIsochoric1Dall.sh
@@ -8,27 +8,27 @@ make DIM=1 USE_MPI=TRUE -j6
 Tp=( 300 )
 tests=( 15 )
 
-python3 setupScripts/setup1D_concClosed.py
+# python3 setupScripts/setup1D_concClosed.py
 
-for i in "${Tp[@]}"
-do
-    for j in "${tests[@]}"
-    do
-	    mpirun -np 16 ./main1d.gnu.MPI.ex inputs amr.n_cell = 512 0 0 euler = 2 viscous = 1 source = 0 particle = 2 Da = 1\
-        enIC = "$j" geometry.prob_lo = 0.0 0.0 0.0 geometry.prob_hi = 0.00512 0.0 0.0 adv.cfl = 0.8 adv.fourier = 0.8\
-        amr.max_level = 0 amr.ref_ratio = 2 2 4 4 amr.probin_file = prbn/detonation amr.plot_int = 1\
-        amr.max_grid_size = 64 amr.plot_files_output = 0 amr.plot_file = detonation/plt stop_time = 0.040\
-        TpInitial = "$i" TgInitial = 300 dp0 = 0.000010 Nsub = 1 conc = 600
-    done
-done
+# for i in "${Tp[@]}"
+# do
+#     for j in "${tests[@]}"
+#     do
+# 	    mpirun -np 16 ./main1d.gnu.MPI.ex inputs amr.n_cell = 512 0 0 euler = 2 viscous = 1 source = 0 particle = 2 Da = 1\
+#         enIC = "$j" geometry.prob_lo = 0.0 0.0 0.0 geometry.prob_hi = 0.00512 0.0 0.0 adv.cfl = 0.8 adv.fourier = 0.8\
+#         amr.max_level = 0 amr.ref_ratio = 2 2 4 4 amr.probin_file = prbn/detonation amr.plot_int = 1\
+#         amr.max_grid_size = 64 amr.plot_files_output = 0 amr.plot_file = detonation/plt stop_time = 0.040\
+#         TpInitial = "$i" TgInitial = 300 dp0 = 0.000010 Nsub = 1 conc = 600
+#     done
+# done
 
-cp -r output/txt/1DflameConfined/isochoric/field/ output/txt/1DflameConfined/600/
-cp -r output/txt/1DflameConfined/isochoric/particle/ output/txt/1DflameConfined/600/
+# cp -r output/txt/1DflameConfined/isochoric/field/ output/txt/1DflameConfined/600/
+# cp -r output/txt/1DflameConfined/isochoric/particle/ output/txt/1DflameConfined/600/
 
-rm output/txt/1DflameConfined/isochoric/field/*.*
-rm output/txt/1DflameConfined/isochoric/particle/*.*
+# rm output/txt/1DflameConfined/isochoric/field/*.*
+# rm output/txt/1DflameConfined/isochoric/particle/*.*
 
-sed -i 's/conc = 600/conc = 700/g' setupScripts/setup1D_concClosed.py
+# sed -i 's/conc = 600/conc = 700/g' setupScripts/setup1D_concClosed.py
 python3 setupScripts/setup1D_concClosed.py
 
 for i in "${Tp[@]}"
--- particleNS/Exec/UniformVelocity/setupScripts/setup1D_concClosed.py
+++ particleNS/Exec/UniformVelocity/setupScripts/setup1D_concClosed.py
@@ -88,7 +88,7 @@ error = 1
 tol = 1e-3
 iteration = 0
 
-conc = 600
+conc = 700
 
 while error > tol:
     avg = 0.5*(NpLo+NpHi)
